9a2d3b7db3852d5124c4e483fedf6b5c161cadf8
seoyeonho <syh2916@kusrc.co.kr>
2025-10-28 13:48:53 +0900

    first commit

---
+++ agent.py
@@ -0,0 +1,31 @@
+# WebAgent/agent.py
+
+from google.adk.agents import Agent 
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.models.lite_llm import LiteLlm
+from WebAgent.confing import constants
+from WebAgent.prompt import ORCHESTRATOR_DESCRIPTION, ORCHESTRATOR_INSTRUCTION
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+# WebPilot Agent Wrapper Tool
+class WebPilotAgentTool(AgentTool):
+    def __init__(self, agent: Agent):
+        super().__init__(agent=agent)
+
+# TODO : WebPilot Agent 별 Tool 생성 
+# planner_tool = WebPilotAgentTool(Planner_Agent)
+# domain_classifier_tool = WebPilotAgentTool(Domain_Priority_Classifier_Agent)
+
+LLM_CLIENT = LiteLlm(model=constants.MODEL_O3_MINI)
+
+root_agent = Agent(
+    name="WebPilot_Orchestrator",
+    model=LLM_CLIENT,
+    description=ORCHESTRATOR_DESCRIPTION,
+    instruction=ORCHESTRATOR_INSTRUCTION,
+    output_key="orchestrator_result"
+)
+
---
+++ confing/constants.py
@@ -0,0 +1,12 @@
+# WebAgent/constants/constants.py
+
+# LLM Models
+MODEL_CLAUDE_SONNET = "anthropic/claude-3-5-sonnet-20240620"
+MODEL_GEMINI_2_5_PRO = "gemini-2.5-pro-preview-03-25"
+MODEL_GEMINI_2_5_FLASH = "gemini-2.5-flash-preview-09-2025"
+MODEL_O3_MINI = "gpt-4o"  # LLM DOM 분석용
+MODEL_GPT_4_1 = "gpt-4o"
+MODEL_GPT_4O = "gpt-4o"  # Vision용
+MODEL_GPT_4O_VISION = "gpt-4o-vision"
+MODEL_GPT_4O_MINI = "gpt-4o-mini"
+MODEL_GPT_4O_MINI_VISION = "gpt-4o-mini-vision"
